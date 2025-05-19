37fca7a1bb3b9f99a00cfee321b84c62fa1a1136
AnnabelleLogan <138072365+AnnabelleLogan@users.noreply.github.com>
2025-05-19 14:37:01 +1000

    person.py
    hopefully it finally committed.

---
+++ person.py
@@ -0,0 +1,42 @@
+class Person():
+    def()__init__(self,fname,lname):
+        self.fname = first
+        self.lname = last
+
+    def PrintFullName(self):
+        return
+
+class parent(Person):
+    super()__init__(self,fname,lname,occupation,alumni,age,graduationYear):
+    self.fname = fname
+    self.lname = lname
+    self.occupation = occupation
+    self.alumni = alumni
+    self.age = age
+    self.graduationYear = graduationYear
+
+class student(Person):
+    super()__init__(self,year,studentID,homeroom):
+    self.year = year
+    self.studentID = studentID
+    self.homeroom = homeroom
+
+    def enrolClass(self):
+        return
+
+class subject(student):
+    super()__init__(self,studentID,subjectName):
+    self.studentID = studentID
+    self.subjectName = subjectName
+
+    def printStudentList(self):
+        return
+
+
+student = student('Annabelle','Logan','Grade: 11',100631,'Broughton')
+
+parent_1 = parent('Mark','Logan','Factory Manager','University of NSW','46','in ancient days')
+parent_2 = parent('Kate','Logan','Supply Chain Stock Manager', 'University of Sydney','48','before the dinosaurs')
+
+studentID = subject(100631)
+subject_List = subject('English','Maths Adv','Maths Ext','Software Engineering','Physics','Chemistry')
